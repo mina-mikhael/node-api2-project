6c6d7fa29419abcbfb242d828bb6a1f1e752d9df
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-08-10 00:15:19 +0200

    initial submission

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+const express = require("express");
+
+const server = express();
+
+const port = 9000;
+
+server.listen(port, () => {
+  console.log(`magic happens here as port ${port} is listening`);
+});
